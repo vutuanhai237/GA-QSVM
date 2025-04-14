9f61243416a1da63860c60707e5bae2e4443fff6
ducto489 <dustnn00@gmail.com>
2025-04-14 11:46:37 +0700

    config training script

--- train1.sh
+++ train1.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#PBS -N Digits_1
+#PBS -N Cancer_1
 #PBS -o /home/nmduc/GA-QSVM/output
 #PBS -e /home/nmduc/GA-QSVM/error
 #PBS -l nodes=1:ppn=8
@@ -11,7 +11,7 @@ module load python3.10
 source ga-qsvm/bin/activate
 
 # Run the main script
-python3.10 main.py --depth 4 --num-circuit 8 --qubits 3 4 5 --num-machines 3 --id 1 --training-size 300 --test-size 0 --data digits
+python3.10 main.py --depth 4 --num-circuit 8 --qubits 3 4 5 --num-machines 3 --id 1 --training-size 300 --test-size 0 --data cancer
 
 
 # Deactivate the virtual environment when done
